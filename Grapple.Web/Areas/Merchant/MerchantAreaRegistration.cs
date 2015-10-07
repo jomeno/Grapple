a3e8f927ca004dbcd2ba764712ad07225995389b
Jomeno <jomeno.vona@gmail.com>
2015-10-07 13:03:19 +0100

    Added Merchant and Principal Areas

---
+++ Grapple.Web/Areas/Merchant/MerchantAreaRegistration.cs
@@ -0,0 +1,25 @@
+﻿using System.Web.Mvc;
+
+namespace Grapple.Web.Areas.Admin
+{
+    public class MerchantAreaRegistration : AreaRegistration
+    {
+        public override string AreaName
+        {
+            get
+            {
+                return "Merchant";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context)
+        {
+            context.MapRoute(
+                "Merchant",
+                "Admin/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional },
+                new[] { "Grapple.Web.Areas.Merchant.Controllers" }
+            );
+        }
+    }
+}
---
+++ Grapple.Web/Areas/Principal/PrincipalAreaRegistration.cs
@@ -0,0 +1,25 @@
+﻿using System.Web.Mvc;
+
+namespace Grapple.Web.Areas.Admin
+{
+    public class PrincipalAreaRegistration : AreaRegistration
+    {
+        public override string AreaName
+        {
+            get
+            {
+                return "Principal";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context)
+        {
+            context.MapRoute(
+                "Principal",
+                "Admin/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional },
+                new[] { "Grapple.Web.Areas.Principal.Controllers" }
+            );
+        }
+    }
+}
